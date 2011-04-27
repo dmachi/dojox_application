27a2285cfe60031753a6f87d03bc0c93a825089d
Dustin Machi <dmachi@dojotoolkit.org>
2011-04-27 12:01:34 -0400

    fix  registerModulePath registration for ./ when endpoint isn't a path

--- application/tests/multiSceneApp/multiSceneApp.js
+++ application/tests/multiSceneApp/multiSceneApp.js
@@ -1,3 +1,11 @@
+var path = window.location.pathname;
+if (path.charAt(path.length)!="/"){
+	path = path.split("/");
+	path.pop();
+	path=path.join("/");	
+}
+dojo.registerModulePath("app",path);
+
 require(["dojo","dojox/application/main", "dojo/text!app/config.json","dojo/json"],function(dojo,Application,config,ref){
 	//app = Application(dojox.json.ref.resolveJson(config), dojo.body());
 	app = Application(dojo.fromJson(config));
