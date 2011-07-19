4a0b3b6206bb0701f892ae4ce73f56dea141040e
Stephen Zhang <yuanpeng.zhang@gmail.com>
2011-07-19 17:25:23 +0800

    make view baseless

--- view.js
+++ view.js
@@ -1,5 +1,5 @@
-define(["dojo", "dijit", "dojox", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin"],function(dojo,dijit,dojox,Widget,Container,Contained,TemplatedMixin,WidgetsInTemplateMixin){
-	return dojo.declare("dojox.app.view", [Widget,TemplatedMixin,Container,Contained, WidgetsInTemplateMixin], {
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin"],function(declare,Widget,Container,Contained,TemplatedMixin,WidgetsInTemplateMixin){
+	return declare("dojox.app.view", [Widget,TemplatedMixin,Container,Contained, WidgetsInTemplateMixin], {
 		selected: false,
 		keepScrollPosition: true,
 		baseClass: "applicationView mblView",
