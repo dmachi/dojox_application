39ac7a84cc82cd5b4d1d373b668650e627c67d36
Stephen Zhang <yuanpeng.zhang@gmail.com>
2011-08-17 15:15:37 +0800

    fix the AMD load in multiSceneApp

--- tests/multiSceneApp/multiSceneApp.js
+++ tests/multiSceneApp/multiSceneApp.js
@@ -1,13 +1,16 @@
-var path = window.location.pathname;
-if (path.charAt(path.length)!="/"){
-	path = path.split("/");
-	path.pop();
-	path=path.join("/");	
-}
-dojo.registerModulePath("app",path);
-console.log('before require', path);
-require(["dojo/_base/html","dojox/app/main", "dojo/text!app/config.json"],function(dojo,Application,config){
-	//app = Application(dojox.json.ref.resolveJson(config), dojo.body());
-	//app = Application(json.parse(config));
-	app = Application(eval("(" + config + ")"));
+require(["dojo/_base/kernel", "dojo/_base/loader"], function(dojo){
+    var path = window.location.pathname;
+    if (path.charAt(path.length)!="/"){
+    	path = path.split("/");
+    	path.pop();
+    	path=path.join("/");	
+    }
+
+    dojo.registerModulePath("app",path);
+    console.log('before require', path);
+    require(["dojo/_base/html","dojox/app/main", "dojo/text!app/config.json"],function(dojo,Application,config){
+    	//app = Application(dojox.json.ref.resolveJson(config), dojo.body());
+    	//app = Application(json.parse(config));
+    	app = Application(eval("(" + config + ")"));
+    });
 });
