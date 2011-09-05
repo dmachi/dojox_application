d9b182eebc7843f99ced72bbf01b89136c8e645b
eric-wang <starwz@gmail.com>
2011-09-05 21:08:55 +0800

    Signed-off-by: eric-wang <starwz@gmail.com>
    publish /app/loadchild event when load child finished.
    application can subscribe this event to do some child node operation like check TabBar, etc.
    
    publish /app/transition event when load child finished.
    application can subscribe this event to do some operation like select TabBarButton, etc.

--- scene.js
+++ scene.js
@@ -1,5 +1,6 @@
 define(["dojo/_base/kernel",
 	"dojo/_base/declare",
+	"dojo/_base/connect",
 	"dojo/_base/array",
 	"dojo/_base/Deferred",
 	"dojo/_base/lang",
@@ -20,7 +21,7 @@ define(["dojo/_base/kernel",
 	"./model", 
 	"./view", 
 	"./bind"], 
-	function(dojo,declare,array,deferred,dlang,has,dstyle,dgeometry,cls,dconstruct,dattr,query,dijit,dojox,WidgetBase,Templated,WidgetsInTemplate,transition, anim, model, baseView, bind){
+	function(dojo,declare,connect, array,deferred,dlang,has,dstyle,dgeometry,cls,dconstruct,dattr,query,dijit,dojox,WidgetBase,Templated,WidgetsInTemplate,transition, anim, model, baseView, bind){
 	
 	var marginBox2contentBox = function(/*DomNode*/ node, /*Object*/ mb){
 		// summary:
@@ -156,6 +157,10 @@ define(["dojo/_base/kernel",
                         bind([child], child.loadedModels);
                     }
 					var addResult = self.addChild(child);
+					//publish /app/loadchild event
+					//application can subscript this event to do user define operation like select TabBarButton, add dynamic script text etc.
+					connect.publish("/app/loadchild", [child]);
+
                  var promise;
 
                  subIds = subIds.split(',');
@@ -543,7 +548,10 @@ define(["dojo/_base/kernel",
 					//a promise object. this.set(...) will handles the this.selectedChild,
 					//activate or deactivate views and refresh layout.
 					this.set("selectedChild", next);
-					//console.log("current.domNode: ", current.domNode, "next.domNode: ", next.domNode);
+					
+					//publish /app/transition event
+					//application can subscript this event to do user define operation like select TabBarButton, etc.
+					connect.publish("/app/transition", [next, toId]);
 					transition(current.domNode,next.domNode,dojo.mixin({},opts,{transition: this.defaultTransition || "none", transitionDefs: transitionDefs})).then(dlang.hitch(this, function(){
 						//dojo.style(current.domNode, "display", "none");
 						if (subIds && next.transition){
