10eac595787bcecd390e7cc7bcf29d8769f73039
Christophe Jolif <cjolif@gmail.com>
2012-05-29 10:50:22 +0200

    Revert "remove stub dojo implementation in scrollable.js because:"
    This reverts commit 3f1f6ae7fcebe2d7544c3ac8758c8e18b44c5432.

--- widgets/scrollable.js
+++ widgets/scrollable.js
@@ -1,3 +1,134 @@
+//>>includeStart("standaloneScrollable", kwArgs.standaloneScrollable);
+
+// The code block surrounded by the includeStart/End pragma is to simulate
+// several dojo APIs that are used in this module for non-dojo applications.
+// For dojo applications, this code block is removed by the build tool.
+
+if(typeof define === "undefined"){ // assumes dojo.js is not loaded
+	dojo = {doc:document, global:window};
+	dojox = {mobile:{}};
+
+	dojo.has = function(name){
+		var ua = navigator.userAgent;
+		if(name === "webkit"){
+			return ua.indexOf("WebKit") != -1;
+		}
+		if(name === "android"){
+			return parseFloat(ua.split("Android ")[1]) || undefined;
+		}
+		if(name === "iphone"){
+			return ua.match(/(iPhone|iPod|iPad)/);
+		}
+		if(name === "ie"){
+			return parseFloat(ua.split("MSIE ")[1]) || undefined;
+		}
+		if(name === "touch"){
+			return (typeof dojo.doc.documentElement.ontouchstart != "undefined" &&
+				navigator.appVersion.indexOf("Mobile") != -1) || !!dojo.has("android");
+		}
+		return null;
+	};
+
+	dojo.stopEvent = function(evt){
+		if(evt.preventDefault){
+			evt.preventDefault();
+			evt.stopPropagation();
+		}else{
+			evt.cancelBubble = true;
+		}
+		return false;
+	};
+
+	dojo.setStyle = function(node, style, value){
+		if(typeof style === "string"){
+			var obj = {};
+			obj[style] = value;
+			style = obj;
+		}
+		for(var s in style){
+			if(style.hasOwnProperty(s)){
+				node.style[s] = style[s];
+				if(s === "opacity" && typeof(node.style.filter) !== "undefined"){
+					node.style.filter = " progid:DXImageTransform.Microsoft.alpha(opacity="+ (style[s]*100) +")";
+				}
+			}
+		}
+	};
+
+	dojo.create = function(tag, attrs, refNode){
+		return refNode.appendChild(dojo.doc.createElement(tag));
+	};
+
+	dojo.hasClass = function(node, s){
+		return (node.className.indexOf(s) != -1);
+	};
+	dojo.addClass = function(node, s){
+		if(!dojo.hasClass(node, s)){
+			node.className += " " + s;
+		}
+	};
+	dojo.removeClass = function(node, s){
+		node.className = node.className.replace(" " + s, "");
+	};
+
+	dojo.connect = function(node, eventName, scope, method){
+		var handler = function(e){
+			e = e || dojo.global.event;
+			if(!e.target){
+				e.target = e.srcElement;
+				e.pageX = e.offsetX;
+				e.pageY = e.offsetY;
+			}
+			scope[method](e);
+		};
+		if(node.addEventListener){
+			node.addEventListener(eventName.replace(/^on/,""), handler, false);
+		}else{
+			node.attachEvent(eventName, handler);
+		}
+		return {node:node, eventName:eventName, handler:handler};
+	};
+	dojo.disconnect = function(handle){
+		if(handle.node.removeEventListener){
+			handle.node.removeEventListener(handle.eventName.replace(/^on/,""), handle.handler, false);
+		}else{
+			handle.node.detachEvent(handle.eventName, handle.handler);
+		}
+	};
+
+	define = function(module, deps, def){
+		var _def = (arguments.length === 2) ? arguments[1] : arguments[2];
+		_def(
+			dojo, // dojo
+			{ // connect
+				connect: dojo.connect,
+				disconnect: dojo.disconnect
+			},
+			{ // event
+				stop: dojo.stopEvent
+			},
+			{ // lang
+				getObject: function(){}
+			},
+			dojo, // win
+			{ // domClass
+				contains: dojo.hasClass,
+				add: dojo.addClass,
+				remove: dojo.removeClass
+			},
+			{ // domConstruct
+				create: dojo.create
+			},
+			{ // domStyle
+				set: dojo.setStyle
+			},
+			dojo.has // has
+		);
+	};
+}
+//>>includeEnd("standaloneScrollable");
+
+
 define([
 	"dojo/_base/kernel",
 	"dojo/_base/connect",
