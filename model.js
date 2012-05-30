ab7ada2ca50ccb87aff8eb77f9580fb0280d2147
Christophe Jolif <cjolif@gmail.com>
2012-05-30 15:13:51 +0200

    fix to avoid new doc parser to fail

--- model.js
+++ model.js
@@ -14,6 +14,7 @@ function(lang, Deferred, when, config){
 		//		available to the view.
 		// returns: loadedModels 
 		//		 loadedModels is an object holding all of the available loaded models for this view.
+
 		//var config = params.config;
 		//var parent = params.parent;
 
