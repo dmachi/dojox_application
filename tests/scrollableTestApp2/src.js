4e2b88902cae5df384040a2e9c346017847d0a0a
Ed Chatelain <ed.chatelain@gmail.com>
2013-02-07 14:36:27 -0500

    more updates the layout support to take a "-" to cleanup the code some, thanks cjolif for the suggestions.

--- tests/scrollableTestApp2/src.js
+++ tests/scrollableTestApp2/src.js
@@ -1,24 +1,17 @@
 require(["dojox/app/main","dojox/app/utils/configUtils",  "dojox/json/ref", "dojo/sniff"],
 	function(Application, configUtils, json, has){
-
-
 	var isTablet = false;
-
 	var configurationFile = "./config-test.json";
 	if(window.innerWidth > 600){
 		isTablet = true;
 	}
-
 	require(["dojo/text!"+configurationFile], function(configJson){
-	
 		var hasList = {};
 		hasList["tablet"] = isTablet;
 		hasList["phone"] = !isTablet;
 		hasList["notIE"] = !has("ie");
 		var config = configUtils.configProcessHas(json.fromJson(configJson),hasList);
 		console.log("back from configProcessHas has config = ",config);
-
-	//	var config = json.fromJson(configJson);
 		config.isTablet = isTablet;
 		Application(config);
 	});
