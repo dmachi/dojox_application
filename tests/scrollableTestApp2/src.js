7474f5096964fa476657e98ec6324a795d5a675b
Ed Chatelain <ed.chatelain@gmail.com>
2013-02-07 08:26:25 -0500

    first drop of hasconfig code to use has parts in the config.

--- tests/scrollableTestApp2/src.js
+++ tests/scrollableTestApp2/src.js
@@ -1,8 +1,29 @@
-require(["dojox/app/main", "dojox/json/ref"],
-	function(Application, json){
+require(["dojox/app/main","dojox/app/utils/configUtils",  "dojox/json/ref", "dojo/sniff"],
+	function(Application, configUtils, json, has){
+
 
 	var isTablet = false;
 
+	var configurationFile = "./config-test.json";
+	if(window.innerWidth > 600){
+		isTablet = true;
+	}
+
+	require(["dojo/text!"+configurationFile], function(configJson){
+	
+		var hasList = {};
+		hasList["tablet"] = isTablet;
+		hasList["phone"] = !isTablet;
+		hasList["notIE"] = !has("ie");
+		var config = configUtils.configProcessHas(json.fromJson(configJson),hasList);
+		console.log("back from configProcessHas has config = ",config);
+
+	//	var config = json.fromJson(configJson);
+		config.isTablet = isTablet;
+		Application(config);
+	});
+
+/*
 	var configurationFile = "./config-phone.json";
 	if(window.innerWidth > 600){
 		configurationFile = "./config-tablet.json";
@@ -14,4 +35,5 @@ require(["dojox/app/main", "dojox/json/ref"],
 		config.isTablet = isTablet;
 		Application(config);
 	});
+*/
 });
