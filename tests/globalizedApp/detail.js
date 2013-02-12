4fe302f2b037a9bd64073d552237876c135330f6
Christophe Jolif <cjolif@gmail.com>
2013-02-12 14:50:08 +0100

    improve paramater naming

--- tests/globalizedApp/detail.js
+++ tests/globalizedApp/detail.js
@@ -1,7 +1,7 @@
 define(["dojo/dom"], function(dom){
 	return {
-		beforeActivate: function(view, data){
-			dom.byId("label").innerHTML = this.nls[view.name]+(data?("-"+data):"");
+		beforeActivate: function(previousView, data){
+			dom.byId("label").innerHTML = this.nls[previousView.name]+(data?("-"+data):"");
 
 		}
 	}
