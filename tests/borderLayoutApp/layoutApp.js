b30dbeb4c00cc8646d4ce9aeb3c3f3baac3f7632
Ed Chatelain <ed.chatelain@gmail.com>
2013-04-02 16:06:54 -0400

    dojox/app cleanup layoutApp test to remove the dojox/mvc code and add an EdgeToEdgeStoreList to the test.

--- tests/borderLayoutApp/layoutApp.js
+++ tests/borderLayoutApp/layoutApp.js
@@ -38,31 +38,6 @@ function(win, Application, jsonRef, config, has){
 			"Fax": "408-764-4321"
 		}]
 	};
-	modelApp.repeatData = [{
-		"First": "Chad",
-		"Last": "Chapman",
-		"Location": "CA",
-		"Office": "1278",
-		"Email": "c.c@test.com",
-		"Tel": "408-764-8237",
-		"Fax": "408-764-8228"
-	}, {
-		"First": "Irene",
-		"Last": "Ira",
-		"Location": "NJ",
-		"Office": "F09",
-		"Email": "i.i@test.com",
-		"Tel": "514-764-6532",
-		"Fax": "514-764-7300"
-	}, {
-		"First": "John",
-		"Last": "Jacklin",
-		"Location": "CA",
-		"Office": "6701",
-		"Email": "j.j@test.com",
-		"Tel": "408-764-1234",
-		"Fax": "408-764-4321"
-	}];
 	var config = jsonRef.fromJson(config);
 	has.add("ie9orLess", has("ie") && (has("ie") <= 9));	
 	config.controllers[2] = "dojox/app/controllers/Layout";		
