aae145c89dd53623a3c85d7f131d99d2b2a699ff
Ed Chatelain <ed.chatelain@gmail.com>
2013-04-05 16:13:13 -0400

    dojox/app new doh tests for layoutApp and mediaQuery3ColumnApp.

--- tests/doh/module.js
+++ tests/doh/module.js
@@ -10,6 +10,8 @@ define([
 		doh.registerUrl("dojox.app.tests.doh.simpleModelApp", require.toUrl("./simpleModelApp/" + userArgs), 999999);
 		doh.registerUrl("dojox.app.tests.doh.globalizedApp", require.toUrl("./globalizedApp/" + userArgs), 999999);
 		doh.registerUrl("dojox.app.tests.doh.borderLayoutApp", require.toUrl("./borderLayoutApp/" + userArgs), 999999);
+		doh.registerUrl("dojox.app.tests.doh.layoutApp", require.toUrl("./layoutApp/" + userArgs), 999999);
+		doh.registerUrl("dojox.app.tests.doh.mediaQuery3ColumnApp", require.toUrl("./mediaQuery3ColumnApp/" + userArgs), 999999);
 	}catch(e){
 		doh.debug(e);
 	}
